15f97b8cee2d6bbf6c5e4a1da1446e8396293bf0
margoglvz <msgalvez06@gmail.com>
2025-03-16 05:44:27 +0000

    starter for dijkstras.cpp and ladder.cpp

---
+++ src/ladder.cpp
@@ -0,0 +1,27 @@
+#include "ladder.h"
+
+bool edit_distance_within(const string& str1, const string& str2, int d) {
+    
+}
+
+bool is_adjacent(const string& word1, const string& word2) {
+    
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    
+}
+
+void load_words(set<string>& word_list, const string& file_name) {
+    
+}
+
+void verify_word_ladder();
+
+void error(string word1, string word2, string msg) {
+    cout << msg << word1 << word2 << endl;
+}
+
+void print_word_ladder(const vector<string>& ladder) {
+    
+}
